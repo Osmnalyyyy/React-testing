879d3efc1376bcb9b56eb9b1ca4a3820b13adaa6
Osmnalyyyy <108487432+Osmnalyyyy@users.noreply.github.com>
2023-01-24 23:30:48 +0300

    v4

--- src/components/todo-input/__test__/todo-input.test.js
+++ src/components/todo-input/__test__/todo-input.test.js
@@ -2,6 +2,8 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import TodoInput from "../todo-input";
 
+const MockedAddTodo = () => {};
+
 it("should render the button as disabled if textbox contains no char", () => {
   render(<TodoInput />);
   const buttonEl = screen.getByRole("button");
@@ -13,7 +15,18 @@ it("should render the button as enabled if textbox contains some chars", () => {
   const buttonEl = screen.getByRole("button");
   const inputEl = screen.getByRole("textbox");
 
-  userEvent.type(inputEl, "hello react");
+  userEvent.type(inputEl, "hello react"); // kullanıcı adına o elemanlarla etkileşime geçmektir buradaki amacımız(kullanıcı ne yaparsa onları tetikleyebiliriz)
   expect(inputEl.value).toBe("hello react");
   expect(buttonEl).toBeEnabled();
+});
+
+it("input should be empty when click the button", () => {
+  render(<TodoInput addTodo={MockedAddTodo} />);
+  const buttonEl = screen.getByRole("button");
+  const inputEl = screen.getByRole("textbox");
+
+  userEvent.type(inputEl, "hello react");
+  userEvent.click(buttonEl);
+  expect(inputEl).toHaveFocus();
+  expect(inputEl.value).toBe("");
 });
