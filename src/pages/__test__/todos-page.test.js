a0c6e632e093b4826ec5db48355f2cc9acc15d59
Osmnalyyyy <108487432+Osmnalyyyy@users.noreply.github.com>
2023-01-25 22:55:50 +0300

    axios

--- src/pages/__test__/todos-page.test.js
+++ src/pages/__test__/todos-page.test.js
@@ -25,4 +25,26 @@ describe("comleted functionality", () => {
     const listItemEl = screen.getByText(/Hi there/i);
     expect(listItemEl).not.toHaveClass("completed");
   });
+
+  it("task should have 'completed' class when clickink on it", () => {
+    render(<TodosPage />);
+    addTask("Hi there");
+    const listItemEl = screen.getByText(/Hi there/i);
+    userEvent.click(listItemEl);
+    expect(listItemEl).toHaveClass("completed");
+  });
+});
+
+describe("deleting functionality", () => {
+  it("task should have been removed when clicking delete button", () => {
+    render(<TodosPage />);
+    addTask("Hi there");
+
+    global.confirm = () => true; // sil butonuna basılınca çıkan confirm fonksiyonunu mocking yaparak hep true dönmesini sagladık
+
+    const listItemEl = screen.getByText(/Hi there/i);
+    const delButton = screen.getByRole("button", { name: "❌" });
+    userEvent.click(delButton);
+    expect(listItemEl).not.toBeInTheDocument();
+  });
 });
